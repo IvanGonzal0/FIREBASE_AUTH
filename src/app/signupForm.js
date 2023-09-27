0648d9e386eb64deecd04f1790b09862bd581344
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-09-27 14:07:48 -0300

    Signup with email and pw

--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,9 +1,27 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
+import { auth } from "./firebase.js"
+
 const signupForm = document.querySelector('#signup-form');
 
-signupForm.addEventListener('submit', (e) => {
+signupForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
   console.log(email, password);
+
+  try{
+    const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+    console.log(userCredentials);
+
+  // CERRAR MODAL
+    const signupModal = document.querySelector('#signupModal');
+    const modal = bootstrap.Modal.getInstance(signupModal);
+    modal.hide();
+  }
+  catch(err){
+    console.log(err);
+  }
+
+
 });
