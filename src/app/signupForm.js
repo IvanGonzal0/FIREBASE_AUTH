53b81ffcdfb1e67aabbdaf1b9046805fc6362ed9
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-09-28 09:32:55 -0300

    Add Toastify js

--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,36 +1,46 @@
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
-import { auth } from "./firebase.js"
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { auth } from "./firebase.js";
+import { showMessage } from "./showMessage.js";
 
-const signupForm = document.querySelector('#signup-form');
+const signupForm = document.querySelector("#signup-form");
 
-signupForm.addEventListener('submit', async (e) => {
+signupForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
 
   console.log(email, password);
 
-  try{
-    const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+  try {
+    // REGISTRAR USUARIO
+    const userCredentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
     console.log(userCredentials);
-
-  // CERRAR MODAL
-    const signupModal = document.querySelector('#signupModal');
+    // CERRAR MODAL
+    const signupModal = document.querySelector("#signupModal");
     const modal = bootstrap.Modal.getInstance(signupModal);
     modal.hide();
-  }
-  catch(err){
-    console.log(err.message);
-    console.log(err.code);
-    if(err.code === 'auth/email-already-in-use'){
-      alert('Email ya registrado');
-    }else if(error.code === 'auth/invalid-email'){
-      alert('Email no válido');
-    }else if(error.code === 'auth/weak-password'){
-      alert('Contraseña débil');
-    }else if(err.code){
-      alert('Error');
+
+    // MOSTRAR MENSAJE
+    showMessage("Bienvenido " + userCredentials.user.email)
+
+  } catch (err) {
+    // CAPTURAR ERRORES
+    // EMAIL REGISTRADO
+    if (err.code === "auth/email-already-in-use") {
+      showMessage("Email ya registrado", "error");
+      // EMAIL NO VALIDO
+    } else if (error.code === "auth/invalid-email") {
+      showMessage("Email no válido", "error");
+      // CONTRASEÑA DEBIL
+    } else if (error.code === "auth/weak-password") {
+      showMessage("Contraseña débil", "error");
+      // ERROR GENERAL
+    } else if (err.code) {
+      showMessage("Error", "error");
     }
   }
-
 });
