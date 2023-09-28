806dab499ef402d1ec1794ca31a60a035f5eeb3b
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-09-28 10:43:05 -0300

    onAuthStateChanged

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
+import { auth } from "./app/firebase.js";
+import { loginCheck } from './app/loginCheck.js'
+
+import './app/logout.js'
 import './app/firebase.js'
 import './app/signupForm.js'
 
 console.log('Hello from main.js');
+
+onAuthStateChanged(auth, async (user) => {
+    loginCheck(user);
+    // if(user) {
+    //     loginCheck(user);
+    // } else {
+    //     loginCheck(user);
+    // }
+});
