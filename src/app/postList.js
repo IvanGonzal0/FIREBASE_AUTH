361cba430eedd149c1a7b531a5dc5d2d635048d8
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-10-02 09:28:08 -0300

    Firestore getData with JS

--- src/app/postList.js
+++ src/app/postList.js
@@ -1,9 +1,26 @@
+const postList = document.querySelector(".posts");
+
 export const setupPosts = (data) => {
+  // VALIDACION DATA NO VACIO
+  if (data.length) {
+    let html = "";
+    // RECORRER DATA Y EXTRAR LOS DATOS CON LA FUNCION .data()
+    data.forEach((doc) => {
+      const post = doc.data();
 
-    if (data.length){
-        console.log('loop posts');
-    }else{
-        console.log('no posts');
-    }
+      // INSERTANDO LOS DATOS EN EL DOM DENTRO DE UNA LISTA ul
+      const li = `
+            <li class="list-group-item list-group-item-action list-group-item-dark">
+                <h5>${post.title}</h5>
+                <p>${post.content}</p>
+            </li>
+        `;
 
-}
+      html += li;
+    });
+    postList.innerHTML = html;
+  } else {
+    postList.innerHTML =
+      '<h2 class="text-center"> There are no posts yet </h2>';
+  }
+};
